f8d01e9c34c1294a6fbe1f6b54befd7fcfffc3d6
Kshitij Kotasthane <kshitij.kotasthane@gmail.com>
2021-12-01 22:03:27 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Navbar, Button } from "react-bootstrap";
+import React, { Component, useState } from "react";
+import { Navbar, Button, Alert, ButtonGroup } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import GoogleButton from "react-google-button";
 import Editor from "react-simple-code-editor";
@@ -8,8 +8,9 @@ import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
+import SplitPane, { Pane } from "react-split-pane";
 
-const code = `
+const codeText = `
   #include <iostream>
 
   /* 
@@ -36,154 +37,186 @@ const code = `
 
 `;
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { editor: false, code, loader: false };
-  }
+const CodeComponent = () => {
+  const [code, setCode] = useState(codeText);
+  const [ac, setAC] = useState(false);
+  const [loader, setLoader] = useState(false);
 
-  render() {
-    return (
-      <>
-        <Navbar bg="light" expand="lg" className="justify-content-center">
-          <Navbar.Brand
-            style={{
-              fontSize: 35,
-            }}
-          >
-            codepilot
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        </Navbar>
-        <>
-          {!this.state.editor ? (
-            <div>
-              <div
-                style={{
-                  position: "relative",
-                }}
-              >
-                <img
-                  style={{
-                    height: "20%",
-                    width: "100%",
-                    opacity: 0.2,
-                  }}
-                  src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
-                />
-                <div
-                  style={{
-                    position: "absolute",
-                    left: "5%",
-                    right: "5%",
-                    bottom: "75%",
-                  }}
-                >
-                  <h1 style={{ color: "black", fontSize: 60 }}>
-                    Prepare for coding interviews in a breeze
-                  </h1>
-                  <div
-                    style={{
-                      paddingTop: 30,
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <GoogleButton
-                      onClick={() => {
-                        this.setState({ loader: true });
-                        setTimeout(() => {
-                          console.log("editor");
-                          this.setState({ editor: true });
-                        }, 2000);
-                      }}
-                    />
-
-                    {this.state.loader ? (
-                      <Loader
-                        type="Rings"
-                        color="black"
-                        height={80}
-                        width={80}
-                      />
-                    ) : null}
-                  </div>
-                </div>
-              </div>
-            </div>
-          ) : (
-            /* Code editor  */
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "stretch",
+  return (
+    <div style={{ padding: "10px" }}>
+      <SplitPane
+        split="vertical"
+        paneStyle={{ width: "500px", height: "200px" }}
+      >
+        <Pane initialSize="500px">
+          <div style={{ display: "flex", justifyContent: "flex-start" }}>
+            <p style={{ padding: 25 }}>
+              <h1>Minimum edit distance</h1>
+              <p>
+                Given two strings str1 and str2 and below operations that can
+                performed on str1.
+              </p>
+              <p>
+                Find minimum number of edits (operations) required to convert
+                ‘str1’ into ‘str2’.
+              </p>
+              <ol>
+                <li>Insert</li>
+                <li>Remove</li>
+                <li>Replace</li>
+              </ol>
+              <p>All of the above operations are of equal cost.</p>
+            </p>
+          </div>
+          <ButtonGroup style={{ padding: "10px" }}>
+            <Button>Find peer</Button>
+            <Button
+              onClick={() => {
+                setLoader(true);
+                setTimeout(() => {
+                  setAC(true);
+                  setLoader(false);
+                }, 2000);
               }}
             >
-              <div style={{ display: "flex", justifyContent: "flex-start" }}>
-                <p style={{ padding: 25 }}>
-                  <h1>Minimum edit distance</h1>
-                  <p>
-                    Given two strings str1 and str2 and below operations that
-                    can performed on str1.
-                  </p>
-                  <p>
-                    Find minimum number of edits (operations) required to
-                    convert ‘str1’ into ‘str2’.
-                  </p>
-                  <ol>
-                    <li>Insert</li>
-                    <li>Remove</li>
-                    <li>Replace</li>
-                  </ol>
-                  <p>All of the above operations are of equal cost.</p>
-                </p>
+              Run Code
+            </Button>
+          </ButtonGroup>
+          {loader ? (
+            <Loader type="TailSpin" color="black" height={60} width={60} />
+          ) : null}
+        </Pane>
+        <Pane initialSize="500px">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Editor
+              value={code}
+              onValueChange={(code) => {
+                setCode(code);
+                console.log(code);
+              }}
+              highlight={(code) => highlight(code, languages.js)}
+              padding={10}
+              style={{
+                height: "50%",
+                width: "85%",
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 12,
+                color: "white",
+                backgroundColor: "black",
+              }}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-end",
+            }}
+          >
+            {ac ? (
+              <div
+                onClick={() => {
+                  setAC(false);
+                }}
+              >
+                <Alert
+                  style={{
+                    width: "725px",
+                  }}
+                  variant="success"
+                >
+                  Your code ran successfully (AC) ✔️
+                </Alert>
               </div>
-              <div>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "flex-end",
+            ) : null}
+          </div>
+        </Pane>
+      </SplitPane>
+    </div>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg" className="justify-content-center">
+      <Navbar.Brand
+        style={{
+          fontSize: 35,
+        }}
+      >
+        codepilot
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    </Navbar>
+  );
+};
+
+const App = () => {
+  const [viewType, setViewType] = useState("home");
+  const [loader, setLoader] = useState(false);
+
+  return (
+    <>
+      <NavBar />
+      {viewType === "home" ? (
+        <div>
+          <div
+            style={{
+              position: "relative",
+            }}
+          >
+            <img
+              style={{
+                height: "20%",
+                width: "100%",
+                opacity: 0.2,
+              }}
+              src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
+            />
+            <div
+              style={{
+                position: "absolute",
+                left: "5%",
+                right: "5%",
+                bottom: "75%",
+              }}
+            >
+              <h1 style={{ color: "black", fontSize: 60 }}>
+                Prepare for coding interviews in a breeze
+              </h1>
+              <div
+                style={{
+                  paddingTop: 30,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <GoogleButton
+                  onClick={() => {
+                    setLoader(true);
+                    setTimeout(() => {
+                      setViewType("code");
+                    }, 2000);
                   }}
-                >
-                  <Editor
-                    value={this.state.code}
-                    onValueChange={(code) => this.setState({ code })}
-                    highlight={(code) => highlight(code, languages.js)}
-                    padding={10}
-                    style={{
-                      height: "50%",
-                      width: "85%",
-                      fontFamily: '"Fira code", "Fira Mono", monospace',
-                      fontSize: 12,
-                      color: "white",
-                      backgroundColor: "black",
-                    }}
-                  />
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "flex-start",
-                  }}
-                >
-                  <Button style={{ marginLeft: "auto", marginRight: "auto" }}>
-                    Run Code
-                  </Button>
-                  <Button
-                    style={{ marginLeft: "auto", marginRight: "auto" }}
-                    disabled
-                  >
-                    Find Peer
-                  </Button>
-                </div>
+                />
+                {loader ? (
+                  <Loader type="Rings" color="black" height={80} width={80} />
+                ) : null}
               </div>
             </div>
-          )}
-        </>
-      </>
-    );
-  }
-}
+          </div>
+        </div>
+      ) : (
+        <CodeComponent />
+      )}
+    </>
+  );
+};
+
+export default App;
